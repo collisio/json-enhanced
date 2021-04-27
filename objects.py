d0717b9d54b0d3e07af003d0456efe619b6ecfdd
Daniel Diego Horcajuelo <d.diegohorcajuelo@collis.io>
2021-04-27 13:55:29 +0200

    remove redundant code

--- objects.py
+++ objects.py
@@ -65,22 +65,18 @@ class JsonList(list, JsonMaster):
 
             if isinstance(item, list):
                 child = JsonList(item)
-                self.__setitem__(i, child)
             elif isinstance(item, dict):
                 child = JsonDict(item)
-                self.__setitem__(i, child)
             elif isinstance(item, str):
                 child = JsonStr(item)
-                self.__setitem__(i, child)
             elif isinstance(item, float):
                 child = JsonFloat(item)
-                self.__setitem__(i, child)
             elif isinstance(item, int):
                 child = JsonInt(item)
-                self.__setitem__(i, child)
             else:
                 raise TypeError(f"Wrong data's format: {type(item)}")
 
+            self.__setitem__(i, child)
             self._child_objects.append(child)
 
 
@@ -97,20 +93,16 @@ class JsonDict(dict, JsonMaster):
 
             if isinstance(value, list):
                 child = JsonList(value)
-                self.__setitem__(key, child)
             elif isinstance(value, dict):
                 child = JsonDict(value)
-                self.__setitem__(key, child)
             elif isinstance(value, str):
                 child = JsonStr(value)
-                self.__setitem__(key, child)
             elif isinstance(value, float):
                 child = JsonFloat(value)
-                self.__setitem__(key, child)
             elif isinstance(value, int):
                 child = JsonInt(value)
-                self.__setitem__(key, child)
             else:
                 raise TypeError(f"Wrong data's format: {type(value)}")
 
+            self.__setitem__(key, child)
             self._child_objects.append(child)
